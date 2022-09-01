4bd9be10d7a82fbf02fcbf15f226b33a36e0bd44
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2022-09-01 17:45:20 -0300

    Pequenos ajustes

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,24 +1,25 @@
 from pytube import YouTube
 
+print("Bem vindo ao YoutuberDownloader")
+print("Aqui voce pode baixar videos do youtube com facilidade")
+
 while True:
-    link = input("Digite o link do vídeo ou 'done' para finalizar o código: ")
+    link = input("Digite o link do vídeo desejado ou 'done' para finalizar o código: ")
     if link == 'done':
             quit()
     try:
         yt = YouTube(link)
     except:
-        print("link invalido")
+        print("link inválido")
         continue
 
     print(("Titulo: ", yt.title))
     print("Autor: ", yt.author)
-    print("Se deseja realizar o download digite 'y', ")
-    print("Se deseja encerrar o codigo digite 'done'")
+    print("Se deseja realizar o download digite 'y'")
     print("Se deseja escolher outro link aperte Enter")
     escolha = input()
     if escolha == 'y':
         print("Downloading...")
         yd = yt.streams.get_highest_resolution()
-        yd.download('./ytDownloads')
-    elif escolha == 'done':
-        quit()
+        yd.download('/Users/User/Downloads')
+        print("Downloaded")
